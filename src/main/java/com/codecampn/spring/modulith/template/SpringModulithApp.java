54f451181392a5ec41a04107b773509402cfdd30
RobinSchneiderCOC <robin.schneider@codecamp-n.com>
2023-10-26 15:28:33 +0200

    add basic spring app

---
+++ src/main/java/com/codecampn/spring/modulith/template/SpringModulithApp.java
@@ -0,0 +1,13 @@
+package com.codecampn.spring.modulith.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModulithApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringModulithApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecampn/spring/modulith/template/SpringModulithAppTests.java
@@ -0,0 +1,13 @@
+package com.codecampn.spring.modulith.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModulithAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
